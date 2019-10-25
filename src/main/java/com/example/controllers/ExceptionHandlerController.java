737fd31b3d7f3da5781473cb9b970fbc5f02ce8f
Szymon <szymonkrd@gmail.com>
2019-10-25 10:15:47 +0200

    JSR 303 - error handling for @Valid(constraints like @NotNull)

--- src/main/java/com/example/controllers/ExceptionHandlerController.java
+++ src/main/java/com/example/controllers/ExceptionHandlerController.java
@@ -2,24 +2,55 @@ package com.example.controllers;
 
 import com.example.exceptions.UniqueConstraintException;
 import com.example.responses.DefaultErrorResponse;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 //globalny generyczny handler dla wyjatku unique costraint
-//tworzymy customowa odpowiedz symulujaca slownik by zwrocic odpowiednia odpowiedz(JSON)
 @ControllerAdvice
-public class ExceptionHandlerController  {
+public class ExceptionHandlerController extends ResponseEntityExceptionHandler {
     @ResponseStatus(HttpStatus.CONFLICT)
     @ExceptionHandler(UniqueConstraintException.class)
     public ResponseEntity<DefaultErrorResponse> exceptionH(UniqueConstraintException exception) {
+        //tworzymy customowa odpowiedz symulujaca slownik by zwrocic odpowiednia odpowiedz(JSON)
         DefaultErrorResponse errors = new DefaultErrorResponse();
         errors.setError(exception.getMessage());
         errors.setStatus(HttpStatus.CONFLICT.value());
 
         return new ResponseEntity<>(errors, HttpStatus.CONFLICT);
+    }
+
+//    In other words, a MethodArgumentNotValidException is thrown when the validation fails.
+//    It is handled by the handleMethodArgumentNotValid() method provided by the
+//    ResponseEntityExceptionHandler, that needs to be overridden if you would like a custom implementation.
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", new Date());
+        body.put("status", status.value());
+
+        //Get all fields errors
+        List<String> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(x -> x.getDefaultMessage())
+                .collect(Collectors.toList());
+
+        body.put("errors", errors);
+
+        return new ResponseEntity<>(body, headers, status);
     }
 }
 
